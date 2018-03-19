0cbce103fd27eb947321477a5d380452e73374c0
Chris Murphy <_chrism_@live.com>
2018-03-19 17:26:01 +0000

    created NameList class with a getNames() method

--- src/main/java/models/NameList.java
+++ src/main/java/models/NameList.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class NameList {
+    public String getNames(){
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("chris");
+        names.add("Seth");
+        names.add("Kevin");
+        names.add("Bradley");
+        names.add("Taha");
+
+        Collections.shuffle(names);
+        return names.get(0);
+
+    }
 }
